8b21137ee9f196111c80fe0bd1a743a18c86b048
Diaconu-Adrian85 <75058478+Diaconu-Adrian85@users.noreply.github.com>
2023-01-19 01:51:50 +0200

    Add files via upload

---
+++ constants/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+    'white': '#ffff',
+    'green': '#06d123',
+    'disableGreen': '#8526d4',
+    'red': '#df216d',
+    'yellow': '#ffd166',
+    'blue': '#118ab2',
+    'green2': '#AAFF00',
+    'red1' : '#FF4500',
+    'dark' : '#000000'
+}
